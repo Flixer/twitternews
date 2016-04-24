e45e5f96216b1e0f77af8654dec7df28b08059b9
mhartwig <mhartwig@it-devcons.de>
2016-04-24 14:35:04 +0200

    Test Commit + Added Getters & Setter

--- src/de/bigdatapraktikum/twitternews/source/Tweet.java
+++ src/de/bigdatapraktikum/twitternews/source/Tweet.java
@@ -6,6 +6,38 @@ public class Tweet {
 	LocalDateTime publishedAt;
 	String source;
 	String content;
+	public LocalDateTime getPublishedAt() {
+		return publishedAt;
+	}
+
+	public void setPublishedAt(LocalDateTime publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getRetweetCount() {
+		return retweetCount;
+	}
+
+	public void setRetweetCount(int retweetCount) {
+		this.retweetCount = retweetCount;
+	}
+
 	int retweetCount;
 
 	public Tweet(LocalDateTime publishedAt, String source, String content, int retweetCount) {

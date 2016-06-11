25666dc52b7ffdafc18d9cef504c4f17b1757028
Hannes <hannes.maeder@gmail.com>
2016-06-11 16:36:03 +0200

    added some buttons (without any functionality)

--- web/code.js
+++ web/code.js
@@ -257,6 +257,31 @@ var initCY = (function() { // on dom ready
 
 }); // on dom ready
 
+$('#date').datepicker({ });
+
 $(function() {
 	FastClick.attach(document.body);
 });
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdownVisibility() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+$('#date').datepicker({ });

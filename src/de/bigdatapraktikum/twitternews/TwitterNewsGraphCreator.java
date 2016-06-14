f6874de733e549bb53b3d93be5b77b905eee1457
mhartwig <mhartwig@it-devcons.de>
2016-06-14 22:07:53 +0200

    Added First Chinese Whisper Implementation

--- src/de/bigdatapraktikum/twitternews/TwitterNewsGraphCreator.java
+++ src/de/bigdatapraktikum/twitternews/TwitterNewsGraphCreator.java
@@ -3,7 +3,11 @@ package de.bigdatapraktikum.twitternews;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import org.apache.flink.api.common.aggregators.Aggregator;
+import org.apache.flink.api.common.aggregators.LongSumAggregator;
 import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
@@ -11,14 +15,23 @@ import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.io.TextOutputFormat.TextFormatter;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.api.java.utils.DataSetUtils;
 import org.apache.flink.core.fs.FileSystem.WriteMode;
 import org.apache.flink.graph.Edge;
 import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.Vertex;
+import org.apache.flink.graph.gsa.ApplyFunction;
+import org.apache.flink.graph.gsa.GatherFunction;
+import org.apache.flink.graph.gsa.Neighbor;
+import org.apache.flink.graph.gsa.SumFunction;
+import org.apache.flink.graph.library.CommunityDetection;
+import org.apache.flink.graph.library.GSAConnectedComponents;
+import org.apache.flink.graph.library.Summarization;
 import org.apache.flink.graph.spargel.MessageIterator;
 import org.apache.flink.graph.spargel.MessagingFunction;
 import org.apache.flink.graph.spargel.VertexUpdateFunction;
+import org.apache.flink.types.IntValue;
+import org.apache.flink.types.LongValue;
+import org.apache.flink.types.MapValue;
 
 import de.bigdatapraktikum.twitternews.processing.ChineseWhisperInitialClassMapper;
 import de.bigdatapraktikum.twitternews.processing.EdgeMapper;
@@ -105,57 +118,88 @@ public class TwitterNewsGraphCreator {
 		verticleList = graph.getVertices().collect();
 //		graph.getVertices().print();
 		Graph<String, Integer, Integer> res = graph.runScatterGatherIteration(new VertexGroupUpdater(),
-				new VertexGroupMessenger(), 1);
+				new VertexGroupMessenger(), 20);
 		res.getVertices().print();
 
 		env.execute();
 	}
+	
+	
 
 	// scatter: messaging
-	public static final class VertexGroupMessenger extends MessagingFunction<String, Integer, Integer, Integer> {
+	public static final class VertexGroupMessenger extends MessagingFunction<String, Integer, Tuple2<Integer, Integer>, Integer> {
 		private static final long serialVersionUID = 1L;
 
 		public void sendMessages(Vertex<String, Integer> v) throws Exception {
-			HashMap<Integer, Integer> groupToEdgesWeightSum = new HashMap<>();
-			Integer maxWeightGroup = null;
-			for (Edge<String, Integer> edge : getEdges()) {
-				String edgeTarget = edge.getTarget();
-				if (edgeTarget.equals(v.getId())) {
-					edgeTarget = edge.getSource();
-				}
-
-				for (Vertex<String, Integer> verticle : verticleList) {
-					if (verticle.getId().equals(edgeTarget)) {
-						int groupId = verticle.getValue();
-						int edgeWeightSum = edge.getValue();
-						if (groupToEdgesWeightSum.containsKey(groupId)) {
-							edgeWeightSum += groupToEdgesWeightSum.get(groupId);
-						}
-						groupToEdgesWeightSum.put(groupId, edgeWeightSum);
-						if (maxWeightGroup == null || groupToEdgesWeightSum.get(maxWeightGroup) < edgeWeightSum) {
-							maxWeightGroup = groupId;
-						}
-						break;
-					}
-				}
-				sendMessageTo(edgeTarget, 0);
+			for(Edge<String, Integer> edge : getEdges()) {
+				sendMessageTo(edge.getTarget(), new Tuple2<>(v.f1,
+						edge.getValue()));
 			}
-			sendMessageTo(v.getId(), new Integer(maxWeightGroup != null ? maxWeightGroup : v.getValue()));
+			
+//			HashMap<Integer, Integer> groupToEdgesWeightSum = new HashMap<>();
+//			Integer maxWeightGroup = null;
+//			for (Edge<String, Integer> edge : getEdges()) {
+//				String edgeTarget = edge.getTarget();
+//				if (edgeTarget.equals(v.getId())) {
+//					edgeTarget = edge.getSource();
+//				}
+//
+//				for (Vertex<String, Integer> verticle : verticleList) {
+//					if (verticle.getId().equals(edgeTarget)) {
+//						int groupId = verticle.getValue();
+//						int edgeWeightSum = edge.getValue();
+//						if (groupToEdgesWeightSum.containsKey(groupId)) {
+//							edgeWeightSum += groupToEdgesWeightSum.get(groupId);
+//						}
+//						groupToEdgesWeightSum.put(groupId, edgeWeightSum);
+//						if (maxWeightGroup == null || groupToEdgesWeightSum.get(maxWeightGroup) < edgeWeightSum) {
+//							maxWeightGroup = groupId;
+//						}
+//						break;
+//					}
+//				}
+//				sendMessageTo(edgeTarget, 0);
+//			}
+//			sendMessageTo(v.getId(), new Integer(maxWeightGroup != null ? maxWeightGroup : v.getValue()));
 		}
 	}
 
 	// gather: vertex update
-	public static final class VertexGroupUpdater extends VertexUpdateFunction<String, Integer, Integer> {
+	public static final class VertexGroupUpdater extends VertexUpdateFunction<String, Integer, Tuple2<Integer, Integer>> {
 		private static final long serialVersionUID = 1L;
 
-		public void updateVertex(Vertex<String, Integer> v, MessageIterator<Integer> m) throws Exception {
-			int groupId = v.getValue();
-			for (Integer mGroupId : m) {
-				if (mGroupId != 0) {
-					groupId = mGroupId;
+		public void updateVertex(Vertex<String, Integer> v, MessageIterator<Tuple2<Integer, Integer>> m) throws Exception {
+			Map<Integer, Integer> receivedGroupsWithScores = new HashMap<Integer, Integer>();
+			
+			
+			for (Tuple2<Integer, Integer> message : m) {
+				Integer group = message.f0;
+				Integer score = message.f1;
+
+				// if it is the first message with the group
+				if (receivedGroupsWithScores.containsKey(group)) {
+					Integer newScore = score + receivedGroupsWithScores.get(group);
+					receivedGroupsWithScores.put(group, newScore);
+				
+				} else {
+					// if group was received before
+					receivedGroupsWithScores.put(group, score);
 				}
 			}
-			setNewVertexValue(groupId);
+			if(!receivedGroupsWithScores.isEmpty()) {
+				Integer maxValue = 0;
+				Integer maxGroup = 0;
+				for (Map.Entry<Integer, Integer> entry : receivedGroupsWithScores.entrySet()) {
+					if(entry.getValue() > maxValue) {
+						maxValue = entry.getValue();
+						maxGroup = entry.getKey();
+					}
+				}
+			
+				setNewVertexValue(maxGroup);
+			}
 		}
 	}
+	
 }
+
